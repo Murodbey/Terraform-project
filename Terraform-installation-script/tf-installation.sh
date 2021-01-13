eedd483fb94fab49e0b566530f0b510a26acc5be
Murodbey <abdullayevmurod@gmail.com>
2021-01-13 13:00:10 -0600

    tf install script updated

--- Terraform-installation-script/tf-installation.sh
+++ Terraform-installation-script/tf-installation.sh
@@ -3,9 +3,9 @@ sudo yum install wget -y
 sudo apt-get install wget -y
 sudo yum install unzip -y
 sudo apt-get install unzip -y 
-wget https://releases.hashicorp.com/terraform/0.11.11/terraform_0.11.11_linux_amd64.zip
-unzip terraform_0.11.11_linux_amd64.zip
+wget https://releases.hashicorp.com/terraform/0.13.0/terraform_0.13.0_linux_amd64.zip
+unzip terraform_0.13.0_linux_amd64.zip
 sudo mv terraform /bin/
-rm terraform_0.11.11_linux_amd64.zip 
+rm terraform_0.13.0_linux_amd64.zip
 terraform --version
 terraform --help
