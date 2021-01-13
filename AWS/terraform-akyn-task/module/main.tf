7ed786a50210c26f8d553bd23cc281e21eb4b130
Murodbey <abdullayevmurod@gmail.com>
2021-01-13 13:21:06 -0600

    updated

--- AWS/terraform-akyn-task/module/main.tf
+++ AWS/terraform-akyn-task/module/main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "nginx" {
   ami                         = var.ami_id
   instance_type               = var.instance_type
   subnet_id                   = var.subnet_id
-  key_name                    = [aws_key_pair.nginx_key.key_name]
+  key_name                    = "aws_key_pair.nginx_key.key_name"
   vpc_security_group_ids      = [aws_security_group.nginx_sg.id]
   associate_public_ip_address = true
 
